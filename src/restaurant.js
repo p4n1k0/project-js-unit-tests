53da43412e5b5091437d1b26514b19cb9d621f0f
Tarick <gabre.tarick@gmail.com>
2022-03-11 00:47:36 -0300

    refatora requisito 10 - parte 1

--- src/restaurant.js
+++ src/restaurant.js
@@ -79,10 +79,6 @@
 // que percorre por todos os itens de `objetoRetornado.consumption`, soma o preço deles e retorna o valor somado acrescido de 10%.
 // DICA: para isso, você precisará percorrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = (callback) => ({ 
-    fetchMenu: () => ({      
-      callback,
-    }),
-});
+const createMenu = (callback) => ({ fetchMenu: () => callback });
 
 module.exports = createMenu;
